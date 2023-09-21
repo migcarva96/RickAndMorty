b8f6ba2c9f50a3e542c2079c2dc1c1ed8adccba8
Miguel Urrea <miguelangeluc0524@gmail.com>
2023-09-21 17:14:07 -0500

    init project

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import 'react-native-gesture-handler';
+
+export const App = () => {
+  return (
+    <View>
+      <View>
+        <Text>Rick and Morty</Text>
+      </View>
+    </View>
+  );
+};
